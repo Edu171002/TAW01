b546abbeb6371cb96112d412a15ec474ce391e30
Edu171002 <emartinezjuarez1710@gmail.com>
2023-10-23 16:40:36 +0200

    Primer commit

---
+++ javascripts/script.js
@@ -0,0 +1,65 @@
+// JavaScript for sidebar navigation
+function openSideNav() {
+    document.querySelector('.side-nav').style.width = '250px';
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeSideNav() {
+    document.querySelector('.side-nav').style.width = '0';
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
+/*
+*
+*/
+// JavaScript for the slideshow container
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n) {
+      slideIndex = n;
+  }
+
+  if (slideIndex > slides.length) {
+      slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+      slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// Initially, start the slideshow
+function startSlideshow() {
+  showSlides();
+}
+
+startSlideshow();
